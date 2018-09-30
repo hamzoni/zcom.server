509e55817c2eb390ea48c9f0c1478fc5c4347a88
ta quy <taquy.se@gmail.com>
2018-09-30 10:28:45 +0700

    ok

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ dotenv.load()
 const app = express()
 	.use(vhost(process.env.SUB_DOMAIN_1, require(process.env.SOURCE_1).app))
 	.use(vhost(process.env.SUB_DOMAIN_2, require(process.env.SOURCE_2).app))
-	.listen(process.env.PORT)
-
 
 // Certificate
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/zcom.io/privkey.pem', 'utf8');
@@ -27,7 +25,6 @@ const httpServer = http.createServer(function (req, res) {
     res.end();
 });
 const httpsServer = https.createServer(credentials, app);
-
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
