a4b8f640a8909b651c9b035b94833b2561b055ba
ta quy <taquy.se@gmail.com>
2018-09-30 10:39:15 +0700

    ok

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ const app = express()
 	.use(vhost(process.env.SUB_DOMAIN_2, require(process.env.SOURCE_2).app))
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/zcom.io/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/zcom.io/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/zcom.io/chain.pem', 'utf8');
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/zcom.io/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/zcom.io/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/zcom.io/chain.pem', 'utf8');
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: ca
+// };
 
 const httpServer = http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
+    // res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    // res.end();
 });
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
